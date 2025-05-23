471b1f262559ed9acf995af2d13555ecfe145f7b
Dung <dungldd325@gmail.com>
2025-05-23 22:21:19 +0700

    cai dat laravel UI, sua giao dien dang ky dang nhap

--- laravel-base/resources/views/auth/login.blade.php
+++ laravel-base/resources/views/auth/login.blade.php
@@ -1,73 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="fxt-form">
+        <h2>Đăng nhập</h2>
+        <p>Đăng nhập để tiếp tục trên trang web của chúng tôi</p>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+            <div class="form-group">
+                <div class="fxt-transformY-50 fxt-transition-delay-1">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                           name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email Address">
+                    @if (!$errors->has('email'))
+                        <i class="flaticon-envelope"></i>
+                    @endif
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                            {{ $message }}
+                        </span>
+                    @enderror
                 </div>
             </div>
-        </div>
+            <div class="form-group">
+                <div class="fxt-transformY-50 fxt-transition-delay-2">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                           name="password" required placeholder="Password" autocomplete="current-password">
+
+                    @if (!$errors->has('password'))
+                        <i class="flaticon-padlock"></i>
+                    @endif
+
+                    @error('password')
+                    <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="fxt-transformY-50 fxt-transition-delay-3">
+                    <div class="fxt-content-between">
+                        <button type="submit" class="fxt-btn-fill">Đăng nhập</button>
+                        <a href="{{ route('password.request') }}" class="switcher-text2">Quên mật
+                            khẩu</a>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
-</div>
 @endsection
--- laravel-base/resources/views/auth/passwords/email.blade.php
+++ laravel-base/resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+    <div class="fxt-form">
+        <h2>Quên mật khẩu</h2>
+        <p>Khôi phục mật khẩu của bạn</p>
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+            <div class="form-group">
+                <div class="fxt-transformY-50 fxt-transition-delay-1">
+                    <input type="email" class="form-control @error('email') is-invalid @enderror"
+                           name="email" value="{{ old('email') }}" placeholder="Email" required autocomplete="email" autofocus>
+                    @if(!$errors->has('email'))
+                        <i class="flaticon-envelope"></i>
                     @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
             </div>
-        </div>
+            <div class="form-group">
+                <div class="fxt-transformY-50 fxt-transition-delay-3">
+                    <button type="submit" class="fxt-btn-fill">Gửi email khôi phục</button>
+                </div>
+            </div>
+        </form>
     </div>
-</div>
 @endsection
